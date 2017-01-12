a5cbb9bc4e2b41ff0db1660cdb67dbd793ea1def
Calvin <soh_calvin@yahoo.com>
2017-01-12 21:15:59 +0800

    asdf

--- java/ref_springboot/src/main/java/csoh/reference/springboot/HelloController.java
+++ java/ref_springboot/src/main/java/csoh/reference/springboot/HelloController.java
@@ -8,12 +8,34 @@ package csoh.reference.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.Iterator;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Bootx!";
+    }
+
+    @RequestMapping("/myip")
+    public String myip(HttpServletRequest req) {
+        return new StringBuilder()
+                .append("{\"ip\" : \"")
+                .append(req.getRemoteAddr())
+                .append("\"}").toString();
+    }
+
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest req) {
+        StringBuilder b = new StringBuilder();
+        for (Enumeration<String> e = req.getHeaderNames(); e.hasMoreElements();) {
+            String h = e.nextElement();
+            b.append(h).append(":").append(req.getHeader(h)).append("<br>");
+        }
+        return b.toString();
     }
 
 }
