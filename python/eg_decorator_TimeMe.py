9e19000bf8ac8e0aec8b1fa10ae8fbd3ecd0f110
Soh <calvin.soh@sap.com>
2017-01-26 16:27:09 +0800

    ---

--- python/eg_decorator_TimeMe.py
+++ python/eg_decorator_TimeMe.py
@@ -1,26 +1,32 @@
 from time import sleep, time
+from functools import wraps
 class TimeMe(object):
 
     def __init__(self, mess):
         self.message = mess
 
     def __call__(self, function_to_be_decorated, *args, **kwargs):
-        def new_func(*args, **kwargs):
+        @wraps(function_to_be_decorated)
+        def wrapper(*args, **kwargs):
             mark = time()
             result = function_to_be_decorated(*args, **kwargs)
             print("Elapsed time {0} : {1}".format(self.message, time() - mark))
             return result
-        return new_func
-
-@TimeMe("Total time of sleepFunction")
-def sleepFunction():
-    print("Start sleeping")
-    sleep(3)
-    print("End sleeping")
+        return wrapper
 
 
+if __name__ == "__main__":
 
-sleepFunction()
+    @TimeMe("Total time of sleepFunction")
+    def sleepFunction():
+        """This is a sleep function"""
+        print("Start sleeping")
+        sleep(1)
+        print("End sleeping")
+        return 1+2
 
+    print(sleepFunction())
+    print(sleepFunction.__name__)
+    print(sleepFunction.__doc__)
 
 
