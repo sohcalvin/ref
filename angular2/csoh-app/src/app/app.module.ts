e4208b951045f37874725de8a86c28f4a32f8bd8
Calvin <soh_calvin@yahoo.com>
2017-04-01 00:11:43 +0800

    --

--- angular2/csoh-app/src/app/app.module.ts
+++ angular2/csoh-app/src/app/app.module.ts
@@ -2,20 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-// import { AlertModule } from 'ng2-bootstrap';
- // ,AlertModule.forRoot()
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent }
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+     BrowserModule
     ,FormsModule
     ,HttpModule
-   
+    ,RouterModule.forRoot(appRoutes)     
    
   ],
   providers: [],
